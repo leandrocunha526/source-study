e00656a13e5f0965eb1c28c6381a33a80fcd7d86
Leandro Cunha <leandrocunha016@gmail.com>
2023-10-10 20:32:27 -0300

    Separate archives in folders

--- calc_imposto.py
+++
@@ -1,9 +0,0 @@
-def calc_imposto(faturamento, taxa):
-    return faturamento * taxa
-
-valores = (1000, 0.3)
-
-faturamento, taxa = valores
-
-imposto = calc_imposto(*valores)
-print(imposto)
--- frutas.py
+++
@@ -1,4 +0,0 @@
-list = ['maça', 'banana', 'melão', 'abacate', 'pera', 'laranja']
-
-for index, list in enumerate(list, start=1):
-    print(index,":",list)
