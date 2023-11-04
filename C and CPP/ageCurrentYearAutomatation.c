8d068c0e88163d51463166f2a4edb18d5535de53
Leandro Cunha <leandrocunha016@gmail.com>
2023-11-04 16:32:47 -0300

    Add files via upload

---
+++ C and CPP/ageCurrentYearAutomatation.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include <time.h>
+
+int calcAge(int age){
+	int calc;
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    int currentYear = tm.tm_year + 1900;
+	calc = currentYear - age;
+	return calc;
+}
+
+int main(){
+  int age;
+  printf("Digite sua idade ");
+  scanf("%d", &age);
+  printf("Seu ano de nascimento acaba de ser calculado! Você nasceu no ano de %d\n", calcAge(age));
+}
